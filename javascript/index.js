955a8ebeff16ab9e9e977f41bf178722116503db
idealab <yuleibest@gmail.com>
2014-11-16 14:29:12 +0800

    commit all the source code

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+(function(){
+    $(".multi-line-text").each(function(i){
+        var divH = $(this).height();
+        var $p = $("p", $(this)).eq(0);
+        while ($p.outerHeight() > divH) {
+            $p.text($p.text().replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "..."));
+        };
+    });
+})();
